8a678a11e5252380a300014566323da244595bdc
BrandonSteffan <brandon.steffan15@gmail.com>
2023-09-20 10:30:53 +0200

    added score display and hit detection

--- script/script.js
+++ script/script.js
@@ -1,6 +1,31 @@
 let target = document.querySelector(".target");
-let posTopT = Math.random()*730;
-let posLeftT = Math.random()*980
+let posTopT = Math.random()*710;
+let posLeftT = Math.random()*960
 target.style.top = posTopT +"px";
 target.style.left = posLeftT+"px";
 target.style.display = "block";
+let score = 0;
+function move(){
+    target.style.display = "none";
+    posTopT = Math.random()*710;
+    posLeftT = Math.random()*960
+    target.style.top = posTopT +"px";
+    target.style.left = posLeftT+"px";
+    target.style.display = "block";
+}
+
+let currentDiff = 1 ;
+nextMove(currentDiff);
+function nextMove(difficulty){
+    setTimeout(() => {
+        move();
+        nextMove(currentDiff);
+    },difficulty*1000);
+}
+
+target.addEventListener("click" , hit());
+
+function hit(){
+    score++;
+    move();
+}
