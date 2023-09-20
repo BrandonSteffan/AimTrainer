00676ee64a19dde5b761e9599c3730fca6d9a1bf
BrandonSteffan <brandon.steffan15@gmail.com>
2023-09-20 10:08:53 +0200

    added random starting position + css fixes

---
+++ script/script.js
@@ -0,0 +1,6 @@
+let target = document.querySelector(".target");
+let posTopT = Math.random()*730;
+let posLeftT = Math.random()*980
+target.style.top = posTopT +"px";
+target.style.left = posLeftT+"px";
+target.style.display = "block";
